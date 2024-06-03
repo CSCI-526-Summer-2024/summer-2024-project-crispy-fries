3b673d34ad98da352d7086692b7d6ab108461a4b
garyya <garyy@usc.edu>
2024-06-02 19:59:16 -0700

    when in shadow, able to dive or jump higher

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,29 @@ public class PlayerControl : MonoBehaviour
     private float Move;
 
     public float jump;
+    public float boostedJump;
 
     public bool isJumping;
 
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    public Transform lightSource;
+    public LayerMask obstacleLayers;
+
+    public bool isInShadow;
+    public bool isDiving;
+    public float normalScale = 1.0f;
+    public float shrinkScale = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        transform.localScale = new Vector3(normalScale, normalScale, normalScale);
+        spriteRenderer.color = Color.white;
     }
 
     // Update is called once per frame
@@ -25,10 +39,17 @@ public class PlayerControl : MonoBehaviour
         Move = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(speed * Move, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && isJumping == false)
+        if (Input.GetButtonDown("Jump") && !isJumping && !isInShadow)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
         }
+        else if (Input.GetButtonDown("Jump") && !isJumping && isInShadow && !isDiving)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, boostedJump));
+        }
+        DetectInShadow();
+        HandleDiveInput();
+        AdjustScale();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -43,6 +64,62 @@ public class PlayerControl : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             isJumping = true;
+        }
+    }
+
+    void DetectInShadow()
+    {
+        Vector3 direction = lightSource.position - transform.position;
+
+        // Check for an obstacle between the object and the light source
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, direction.magnitude, obstacleLayers);
+
+        Debug.DrawRay(transform.position, direction, Color.red);
+
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Obstacle detected: " + hit.collider.name);
+            isInShadow = true;
+            spriteRenderer.color = Color.black;
+        }
+        else
+        {
+            // No obstacle detected
+            Debug.Log("Obstacle not detected");
+            isInShadow = false;
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    void HandleDiveInput()
+    {
+        if (!isInShadow)
+        {
+            isDiving = false;
+        }
+        else if (isInShadow && !isDiving && !isJumping && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)))
+        {
+            isDiving = true;
+        }
+        else if (isInShadow && isDiving && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)))
+        {
+            isDiving = false;
+        }
+
+        
+    }
+
+    void AdjustScale()
+    {
+        if (isDiving)
+        {
+            transform.localScale = new Vector3(normalScale, shrinkScale, normalScale);
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(normalScale, normalScale, normalScale);
         }
     }
 }
