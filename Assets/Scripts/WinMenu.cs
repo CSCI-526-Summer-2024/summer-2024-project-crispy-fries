631d93c28530d885a6260d9103d38f4cc8c1d248
garyya <garyy@usc.edu>
2024-06-17 16:32:28 -0700

    fix resume button

--- Assets/Scripts/WinMenu.cs
+++ Assets/Scripts/WinMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.SceneManagement;
 
 public class WinMenu : MonoBehaviour
@@ -9,12 +10,20 @@ public class WinMenu : MonoBehaviour
 
     public GameObject WinMenuUI;
 
+    private string sceneName;
+    private int levelNumber;
 
     // Start is called before the first frame update
     void Start()
     {
         WinMenuUI.SetActive(false);
-        
+
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        sceneName = currentScene.name;
+
+        // Extract the level number
+        levelNumber = GetLevelNumber(sceneName);
     }
 
 
@@ -28,10 +37,46 @@ public class WinMenu : MonoBehaviour
 
     }
 
+    int GetLevelNumber(string sceneName)
+    {
+        // Assuming the scene name format is "level X" where X is the level number
+        string[] parts = sceneName.Split(' ');
+        if (parts.Length > 1)
+        {
+            int.TryParse(parts[1], out int levelNumber);
+            return levelNumber;
+        }
+        return -1;
+    }
+
     public void Resume()
     {
-        
+        string nextLevelName = "Level " + (levelNumber + 1);
+
+        if (IsLevelInBuildSettings(nextLevelName))
+        {
+            SceneManager.LoadScene(nextLevelName);
+        }
+        else
+        {
+            // If the next level does not exist, load the Level Selection scene
+            SceneManager.LoadScene("Level Selection");
+        }
         LevelIsComplete = false;
+    }
+    private bool IsLevelInBuildSettings(string levelName)
+    {
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        for (int i = 0; i < sceneCount; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+            if (sceneName == levelName)
+            {
+                return true;
+            }
+        }
+        return false;
     }
     public void LoadMenu()
     {
@@ -40,7 +85,7 @@ public class WinMenu : MonoBehaviour
     }
     public void Restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(sceneName);
         LevelIsComplete = false;
     }
 }
