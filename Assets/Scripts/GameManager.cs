6bc44abe8e8d3bd0b28d840f5f226c270dff3504
Suvi <aamarath@usc.edu>
2024-06-20 14:27:27 -0700

    set framerate, change build name

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,13 +8,13 @@ public class GameManager : MonoBehaviour
     public TextUIManager textUIManager;
     public LevelManager levelManager;
 
-    public string buildName = "Alpha Prototype";
+    public string buildName = "Post Alpha Prototype Thursday";
 
     public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
