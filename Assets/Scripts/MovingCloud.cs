57bc0f7d680876273377f9ac9ef960ed43bfbdaf
AAAAAiden <aaaidenlin@gmail.com>
2024-06-25 13:14:50 -0700

    adding jumping to cloud

--- Assets/Scripts/MovingCloud.cs
+++ Assets/Scripts/MovingCloud.cs
@@ -6,14 +6,20 @@ public class MovingPlatform : MonoBehaviour
 {
     public Transform posA, posB;
     public float speed;
+    public bool moving;
     Vector3 targetPos;
 
     private void Start()
     {
         targetPos = posB.position;
+        moving = false;
     }
     void Update()
     {
+        if (!moving)
+        {
+            return;
+        }
         if(Vector2.Distance(transform.position, posA.position) < 0.05f)
         {
             targetPos = posB.position;
@@ -24,6 +30,9 @@ public class MovingPlatform : MonoBehaviour
         }
         transform.position = Vector3.MoveTowards(transform.position, targetPos, speed* Time.deltaTime);
 
+    }
+    public void startMoving() {
+        moving = true;
     }
     
 }
