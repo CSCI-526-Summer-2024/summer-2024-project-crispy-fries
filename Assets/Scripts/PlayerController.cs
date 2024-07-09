216d9faa32d42da9e0bb7b77fc31c231196e43e3
Suvi <aamarath@usc.edu>
2024-07-08 18:37:29 -0700

    shake on SD in colored light

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -183,7 +183,8 @@ public class PlayerController : MonoBehaviour
         {
             SetStateShadowDive();
         }
-        else if (!gameManager.GameIsPaused && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && !isInLight && isGrounded && feetOn == FloorType.Ground && floorMaterial == FloorMaterial.GlassTile)
+        else if ((!gameManager.GameIsPaused && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && !isInLight && isGrounded && feetOn == FloorType.Ground && floorMaterial == FloorMaterial.GlassTile) || 
+        (!gameManager.GameIsPaused && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && isInLight && isGrounded && feetOn == FloorType.Ground))
         {
             // trying to dive on glass
             //StartCoroutine(cameraShake.Shake(0.3f, 0.1f));
@@ -378,7 +379,7 @@ public class PlayerController : MonoBehaviour
 
 
        
-        if (!gameManager.GameIsPaused  && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))&& canTransformToNormal())
+        if (!gameManager.GameIsPaused  && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && canTransformToNormal())
         {
             SetStateNormal();
             return;
