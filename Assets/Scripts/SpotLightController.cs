c27d3f08dab50297e36a0c00975ade1dfe0d2c11
jungkyuk <jungkyuk@usc.edu>
2024-07-14 15:03:12 -0700

    Updated rotating lights

--- Assets/Scripts/SpotLightController.cs
+++ Assets/Scripts/SpotLightController.cs
@@ -32,6 +32,9 @@ public class SpotLightController : MonoBehaviour
     [SerializeField]
     private GameObject timerProgress;
     private Coroutine countdownCoroutine;
+    public int rotationAngleFrom;
+    public int rotationAngleTo;
+    public bool doesRotate;
 
 
     
