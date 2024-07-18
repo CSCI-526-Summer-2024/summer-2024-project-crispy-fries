8c68b1f5fddc60bc6cb7c9ae39bc185c06aa6532
jungkyuk <jungkyuk@usc.edu>
2024-07-17 21:00:56 -0700

    Changed all spotlight colors to only blue and yellow

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -31,10 +31,12 @@ public class LevelManager : MonoBehaviour
         if (currBuildIndex >= SceneManager.sceneCountInBuildSettings - 1) {
             // At last scene, you win!
             //textUIManager.WinGame();
+            // SceneManager.UnloadSceneAsync(currBuildIndex);
             SceneManager.LoadSceneAsync(0); // Load select screen
             pauseInput=true;
         }
         else {
+            // SceneManager.UnloadSceneAsync(currBuildIndex);
             SceneManager.LoadSceneAsync(++currBuildIndex);
         }
     }
